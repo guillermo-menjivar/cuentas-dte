b062f288edb3e01c1dd11c2aed7f46dee55646ba
guillermo-menjivar <gmenjivar4@gmail.com>
2025-09-27 18:14:39 -0600

    updates

---
+++ artifacts/tools/test_authentication.sh
@@ -0,0 +1,4 @@
+curl -X POST "https://apitest.dtes.mh.gob.sv/seguridad/auth" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -H "User-Agent: user" \
+  -d "user=${USERNAME}&pwd=${PASSWORD}"
