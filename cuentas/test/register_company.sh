744f966bbbf5fbd14efd2d5049cd1960ddb68de9
guillermo-menjivar <gmenjivar4@gmail.com>
2025-09-29 10:18:07 -0600

    updates

---
+++ cuentas/test/register_company.sh
@@ -0,0 +1,10 @@
+curl -X POST http://localhost:8080/v1/companies \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "Paredes & Paredes, S.A. de C.V",
+    "nit": "0614-300506-101-3",
+    "ncr": "172631-3",
+    "hc_username": "0614-300506-101-3",
+    "hc_password": "MF7HwttFuZ.*3RY",
+    "email": "contact@paredes.com"
+  }'
