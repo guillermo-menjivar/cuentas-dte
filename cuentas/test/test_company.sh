ecfcc7f47d8901dee3af1eb83d6b707392cf8ab6
guillermo-menjivar <gmenjivar4@gmail.com>
2025-09-28 19:09:45 -0600

    updates

---
+++ cuentas/test/test_company.sh
@@ -0,0 +1,10 @@
+curl -X POST http://localhost:8080/v1/companies \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "Acme Corporation",
+    "nit": 12345678901234,
+    "ncr": 98765432109876,
+    "hc_username": "acme_user",
+    "hc_password": "super_secret_password_123",
+    "email": "contact@acme.com"
+  }' | jq
