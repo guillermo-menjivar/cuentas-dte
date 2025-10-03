ae8cb28cddf1aff668c0821f8c206e2e22fd5f32
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-03 10:41:05 -0600

    updates

--- cuentas/migrations/0006_create_inventory_items_table.down.sql
+++ cuentas/migrations/0006_create_inventory_items_table.down.sql
@@ -1,6 +1,3 @@
--- Drop trigger and function
 DROP TRIGGER IF EXISTS trigger_update_inventory_items_updated_at ON inventory_items;
 DROP FUNCTION IF EXISTS update_inventory_items_updated_at();
-
--- Drop table
 DROP TABLE IF EXISTS inventory_items;
