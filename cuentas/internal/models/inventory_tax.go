17c72996e2fff06910af8e67168c4760b46412f8
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-03 10:50:06 -0600

    updates

---
+++ cuentas/internal/models/inventory_tax.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"fmt"
+	"time"
+
+	"cuentas/pkg/codigos"
+)
+
+// InventoryItemTax represents a tax associated with an inventory item
+type InventoryItemTax struct {
+	ID          string    `json:"id"`
+	ItemID      string    `json:"item_id"`
+	TributoCode string    `json:"tributo_code"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+// AddItemTaxRequest represents a request to add a tax to an item
+type AddItemTaxRequest struct {
+	TributoCode string `json:"tributo_code" binding:"required"`
+}
+
+// Validate validates the add item tax request
+func (r *AddItemTaxRequest) Validate() error {
+	// Validate tributo code exists in our codigos package
+	if !codigos.IsValidTributo(r.TributoCode) {
+		return fmt.Errorf("invalid tributo_code: %s", r.TributoCode)
+	}
+
+	return nil
+}
