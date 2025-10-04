aa1bbd183d70a7fcff37b1cd5a1cd9695b8c0f83
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-04 07:43:27 -0600

    uopdates

---
+++ cuentas/migrations/0013_seed_consumidor_final_clients.up.sql
@@ -0,0 +1,72 @@
+-- Create consumidor final client for each existing company
+INSERT INTO clients (
+    company_id,
+    nit,
+    business_name,
+    legal_business_name,
+    giro,
+    tipo_contribuyente,
+    tipo_persona,
+    full_address,
+    country_code,
+    department_code,
+    municipality_code,
+    active
+)
+SELECT 
+    id as company_id,
+    9999999999999 as nit,
+    'Consumidor Final' as business_name,
+    'Consumidor Final' as legal_business_name,
+    'Consumidor Final' as giro,
+    'Consumidor Final' as tipo_contribuyente,
+    '1' as tipo_persona,
+    'N/A' as full_address,
+    'SV' as country_code,
+    '06' as department_code,
+    '0623' as municipality_code,
+    true as active
+FROM companies
+ON CONFLICT DO NOTHING;
+
+-- Create trigger to automatically create consumidor final for new companies
+CREATE OR REPLACE FUNCTION create_consumidor_final_for_company()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO clients (
+        company_id,
+        nit,
+        business_name,
+        legal_business_name,
+        giro,
+        tipo_contribuyente,
+        tipo_persona,
+        full_address,
+        country_code,
+        department_code,
+        municipality_code,
+        active
+    ) VALUES (
+        NEW.id,
+        9999999999999,
+        'Consumidor Final',
+        'Consumidor Final',
+        'Consumidor Final',
+        'Consumidor Final',
+        '1',
+        'N/A',
+        'SV',
+        '06',
+        '0623',
+        true
+    );
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_create_consumidor_final
+    AFTER INSERT ON companies
+    FOR EACH ROW
+    EXECUTE FUNCTION create_consumidor_final_for_company();
+
+COMMENT ON FUNCTION create_consumidor_final_for_company IS 'Automatically creates a Consumidor Final client for each new company';
