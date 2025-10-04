6091a0c2fd4f743d723b76477c084bafa1359286
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-04 06:55:07 -0600

    updates

---
+++ cuentas/internal/codigos/special_clients.go
@@ -0,0 +1,13 @@
+package codigos
+
+// Special client identifiers for system-wide use
+const (
+	// ConsumidorFinalName is the standard name for anonymous retail customers
+	ConsumidorFinalName = "Consumidor Final"
+
+	// ConsumidorFinalLegalName is the legal name for anonymous customers
+	ConsumidorFinalLegalName = "Consumidor Final"
+
+	// ConsumidorFinalNIT is the special NIT used for consumidor final in El Salvador DTEs
+	ConsumidorFinalNIT = "9999999999999"
+)
