5553c84b98a431a67466e5dc2e7c62fdf0119d03
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-04 17:23:14 -0600

    updates

---
+++ cuentas/test/test_list_inventory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Usage: ./test_list_inventory.sh <company_id>
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <company_id>"
+    exit 1
+fi
+
+COMPANY_ID=$1
+API_URL="http://localhost:8080/v1/inventory/items"
+
+echo "Listing inventory items..."
+echo "Company ID: $COMPANY_ID"
+echo ""
+
+curl -s -X GET "$API_URL" \
+    -H "X-Company-ID: $COMPANY_ID" | jq '.'
