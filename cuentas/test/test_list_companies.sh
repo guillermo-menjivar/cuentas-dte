cbdfa758f971e309cb2304acd9ca4b2ceca84947
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-04 18:35:35 -0600

    updates

---
+++ cuentas/test/test_list_companies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Usage: ./test_list_companies.sh
+
+API_URL="http://localhost:8080/v1/companies"
+
+echo "Listing all companies..."
+echo ""
+
+# Note: This endpoint might not exist in your current API
+# If it doesn't, you'll need to add it to your handlers
+
+curl -s -X GET "$API_URL" | jq '.'
