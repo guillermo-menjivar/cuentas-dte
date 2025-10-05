7120239989356027e17cb9838cf7ab449c608dcc
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-05 13:52:11 -0600

    updates

---
+++ cuentas/test/test_get_categories.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# File: test_get_categories.sh
+# Usage: ./test_get_categories.sh
+# Description: Lists all top-level economic activity categories
+
+API_URL="http://localhost:8080/v1/actividades-economicas/categories"
+
+echo "Fetching all economic activity categories..."
+echo ""
+
+curl -s -X GET "$API_URL" | jq '.'
