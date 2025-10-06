f0aea9101fe40f492fa8edee11041beebdaad103
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-06 11:45:06 -0600

    updates

---
+++ cuentas/internal/services/actividad_economica_helpers.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"cuentas/internal/codigos"
+	"sort"
+)
+
+func sortCategoriesByCode(categories []codigos.ActivityCategory) []codigos.ActivityCategory {
+	sort.Slice(categories, func(i, j int) bool {
+		return categories[i].Code < categories[j].Code
+	})
+	return categories
+}
+
+func sortActivitiesByCode(activities []codigos.EconomicActivity) []codigos.EconomicActivity {
+	sort.Slice(activities, func(i, j int) bool {
+		return activities[i].Code < activities[j].Code
+	})
+	return activities
+}
+
+func sortByRelevance(results []SearchResult) []SearchResult {
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].RelevanceScore > results[j].RelevanceScore
+	})
+	return results
+}
