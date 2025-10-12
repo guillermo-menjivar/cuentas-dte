f101541ba6d38816b7585e14f45a7256be8f1f11
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-12 13:16:06 -0600

    updates

--- cuentas/test/test_firmador_manually.sh
+++ cuentas/test/test_firmador_manually.sh
@@ -1,3 +1,4 @@
 curl -X POST http://167.172.230.154:8113/firmardocumento/ \
   -H "Content-Type: application/json" \
-  -d @test_firmador.json | jq .
+  -d @test_simple_firmador.json | jq .
+  #-d @test_firmador.json | jq .
