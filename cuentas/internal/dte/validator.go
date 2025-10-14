873195652514e5be8cd52b2a6686c6fcc8a22591
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-13 20:02:11 -0600

    updates

---
+++ cuentas/internal/dte/validator.go
@@ -0,0 +1,22 @@
+package dte
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	// Updated to include B and P for establishments
+	establishmentCodeRegex = regexp.MustCompile(`^[MSBP][0-9]{3}$`)
+	posCodeRegex           = regexp.MustCompile(`^P[0-9]{3}$`)
+	numeroControlRegex     = regexp.MustCompile(`^DTE-[0-9]{2}-[MSBP][0-9]{3}P[0-9]{3}-[0-9]{15}$`)
+)
+
+// ValidateEstablishmentCode validates establishment code format
+// Valid: M001, S001, B001, P001
+func ValidateEstablishmentCode(code string) error {
+	if !establishmentCodeRegex.MatchString(code) {
+		return fmt.Errorf("invalid establishment code format: %s (expected M###, S###, B### or P###)", code)
+	}
+	return nil
+}
