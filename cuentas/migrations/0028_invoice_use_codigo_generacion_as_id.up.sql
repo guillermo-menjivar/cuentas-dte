8285fc8bcfdddfb7951e63f9b00754e29a4fb833
guillermo-menjivar <gmenjivar4@gmail.com>
2025-10-14 19:23:10 -0600

    updats

--- cuentas/migrations/0028_invoice_use_codigo_generacion_as_id.up.sql
+++ cuentas/migrations/0028_invoice_use_codigo_generacion_as_id.up.sql
@@ -1,4 +1,6 @@
+ALTER TABLE invoices DROP CONSTRAINT invoices_references_invoice_id_fkey;
 ALTER TABLE invoices ALTER COLUMN id TYPE VARCHAR(36);
 ALTER TABLE invoices ALTER COLUMN references_invoice_id TYPE VARCHAR(36);
+ALTER TABLE invoices ADD CONSTRAINT invoices_references_invoice_id_fkey FOREIGN KEY (references_invoice_id) REFERENCES invoices(id);
 
 
