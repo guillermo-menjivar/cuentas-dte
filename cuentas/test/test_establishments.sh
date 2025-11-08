be2a56ae1c863a7e0555bbf9a5c622e929051f8b
guillermo-menjivar <gmenjivar4@gmail.com>
2025-11-07 23:50:35 -0600

    updates

--- cuentas/test/test_establishments.sh
+++ cuentas/test/test_establishments.sh
@@ -47,7 +47,7 @@ CREATE_ESTABLISHMENT_RESPONSE=$(curl -s -X POST "$BASE_URL/establishments" \
   -d '{
     "tipo_establecimiento": "02",
     "nombre": "Casa Matriz - San Salvador",
-    "cod_establecimiento": "0029",
+    "cod_establecimiento": "0038",
     "departamento": "06",
     "municipio": "14",
     "complemento_direccion": "Colonia Escalón, Calle Principal #123, San Salvador",
@@ -74,7 +74,7 @@ CREATE_SUCURSAL_RESPONSE=$(curl -s -X POST "$BASE_URL/establishments" \
   -d '{
     "tipo_establecimiento": "01",
     "nombre": "Sucursal Santa Ana",
-    "cod_establecimiento": "0034",
+    "cod_establecimiento": "0084",
     "departamento": "13",
     "municipio": "01",
     "complemento_direccion": "Centro Comercial Metrocentro, Local 45, Santa Ana",
