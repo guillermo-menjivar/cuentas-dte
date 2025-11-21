423a7a49d98174b863fdb0381b09034dc241724b
guillermo-menjivar <gmenjivar4@gmail.com>
2025-11-21 13:27:25 -0500

    updates

--- cuentas/internal/dte/service_notas.go
+++ cuentas/internal/dte/service_notas.go
@@ -117,8 +117,10 @@ func (s *DTEService) ProcessNotaDebito(ctx context.Context, nota *models.NotaDeb
 		} else {
 			fmt.Println("✅ Hacienda response saved to nota")
 		}
-		UploadDTEToS3Async(dteJSON, false, codigos.DocTypeNotaDebito, nota.CompanyID, strings.ToUpper(nota.ID))
-		UploadDTEToS3Async([]byte(signedDTE), true, codigos.DocTypeNotaDebito, nota.CompanyID, strings.ToUpper(nota.ID))
+		UploadDTEToS3Async(dteJSON, "unsigned", codigos.DocTypeNotaDebito, nota.CompanyID, strings.ToUpper(nota.ID))
+		UploadDTEToS3Async([]byte(signedDTE), "signed", codigos.DocTypeNotaDebito, nota.CompanyID, strings.ToUpper(nota.ID))
+		haciendaResponseJSON, _ := json.MarshalIndent(response, "", "  ")
+		UploadDTEToS3Async(haciendaResponseJSON, "hacienda_response", codigos.DocTypeNotaDebito, nota.CompanyID, strings.ToUpper(nota.ID))
 	}
 
 	// Step 8: Log to commit log
